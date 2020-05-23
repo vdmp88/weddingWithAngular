2d82c92c24ef10f1f95ddcd19cc03b28e15a1488
vdmp <vdmp@MacBook-Pro-vdmp.local>
2020-05-23 21:24:13 +0300

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HamburgerComponent } from './components/hamburger/hamburger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HamburgerComponent
   ],
   imports: [
     BrowserModule,
